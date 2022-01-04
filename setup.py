4126cc012908675db389ff592f423c878d22b36c
Srinath Murali <91878714+SrinathMuraliN@users.noreply.github.com>
2022-01-04 11:51:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 import os
 import os.path as op
-from setuptools import PEP420PackageFinder
 from distutils.core import setup
+
+from setuptools import PEP420PackageFinder
 
 ROOT = op.dirname(op.abspath(__file__))
 SRC = op.join(ROOT, "src")
 
 
 def get_version_info():
-    """ Extract version information as a dictionary from version.py. """
+    """Extract version information as a dictionary from version.py."""
     version_info = {}
-    version_filename = os.path.join("src", "sephora_lib", "version.py")
+    version_filename = os.path.join("src", "magento_lib", "version.py")
     with open(version_filename, "r") as version_module:
         version_code = compile(version_module.read(), "version.py", "exec")
     exec(version_code, version_info)
@@ -18,10 +19,10 @@ def get_version_info():
 
 
 setup(
-    name="sephora_lib",
+    name="magento_lib",
     version=get_version_info()["version"],
     package_dir={"": "src"},
-    description="Sephora ML Workflow",
-    author="Sephora",
+    description="Magento ML Workflow",
+    author="Magento",
     packages=PEP420PackageFinder.find(where=str(SRC)),
 )
