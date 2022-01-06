e89115b5f4ef23e77250c90f88509a4ef8f52671
Srinath Murali <91878714+SrinathMuraliN@users.noreply.github.com>
2022-01-06 14:35:30 +0530

    Update call_jobs.py

--- production/call_jobs.py
+++ production/call_jobs.py
@@ -2,7 +2,8 @@ import logging
 import traceback
 
 import click
-from magento_lib.core.context import create_context
+import fsspec
+import yaml
 from magento_lib.inference.model_inference import start_inference
 from magento_lib.preprocessing.data_loading import load_data
 from magento_lib.preprocessing.data_preprocessing import start_preprocessing
@@ -12,54 +13,63 @@ from magento_lib.training.model_training import start_training
 @click.group()
 @click.option("-tc", "--training_config", required=True)
 @click.pass_context
-def cli():
+def cli(ctx, training_config):
     print("calling cli")
-    training_config = (
-        "gs://pg-explore/data/magento/interim/argentina_parameters.yml"
-    )
-    context = create_context(training_config)
-    session = context.CustomSparkSession(context.config)
-    session.CreateSparkSession()
-    spark = session.spark
-    context.spark = spark
-    print("Setting context to object")
-    ctx.obj["context"] = context
+    interim_fs = gcsfs.GCSFileSystem(project="tiger-mle")
+    with interim_fs.open(training_config, "r") as fp:
+        params = yaml.safe_load(fp)
+    ctx.obj["context"] = params
 
 
 @cli.command()
 @click.pass_context
-def load_data(ctx):
+def data_loading():
+    print("Starting data_loading ***")
     load_data()
+    print("Ending data_loading ***")
     return click.echo("success")
 
 
 @cli.command()
 @click.pass_context
-def start_preprocessing(ctx):
+def data_preprocessing(ctx):
     context = ctx.obj["context"]
-    # start_preprocessing(context, context.spark, ctx.obj["mlflow_run_suffix"])
-    start_preprocessing()
+    print("Starting data_preprocessing ***")
+    start_preprocessing(context)
+    print("Ending data_preprocessing ***")
     return click.echo("success")
 
 
 @cli.command()
 @click.pass_context
-def start_training(ctx):
+def model_training(ctx):
     context = ctx.obj["context"]
-    # start_training(context, context.spark, ctx.obj["mlflow_run_suffix"])
-    start_training()
+    start_training(context)
     return click.echo("success")
 
 
 @cli.command()
 @click.pass_context
-def start_inference():
-    # context = ctx.obj["context"]
-    # start_inference(context, context.spark, ctx.obj["mlflow_run_suffix"])
+def model_inference():
+    context = ctx.obj["context"]
     print("Starting the Inference Method")
-    start_inference()
+    start_inference(context)
     print("Ending the Inference Method")
     return click.echo("success")
+
+
+@cli.command()
+@click.pass_context
+def main(ctxt=None):
+    ctxt = ctxt or {}
+    try:
+        cli(obj={})
+    except click.exceptions.Exit as e:
+        if e.code != 0:
+            raise
+    except SystemExit as e:
+        if e.code != 0:
+            raise
 
 
 if __name__ == "__main__":
